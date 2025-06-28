7f291d096ce725b9f96680d31b735d0c94e16e4d
samadev-45 <samadev45@gmail.com>
2025-06-28 14:38:48 +0530

    Time: 12 ms (93.24%) | Memory: 73.5 MB (73.03%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
-  let sett=new Set()
-  for(let num of nums){
-     if(sett.has(num)){
-      return true;
-     }
-      sett.add(num)
-     
-     }
-     return false;
-  
+    const set = new Set();
+    for(let i=0; i<nums.length;i++){
+        if(set.has(nums[i])){
+            return true
+        }
+        set.add(nums[i])
     }
+    return false  
+};
