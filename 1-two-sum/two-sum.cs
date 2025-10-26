4d00e7ed0d4e6edd549eb0dfb9e4d7ec16bcad54
Samadev A <samadev45@gmail.com>
2025-10-26 19:35:50 +0530

    Time: 1 ms (99.05%) | Memory: 49.4 MB (16.31%) - LeetSync

---
+++ 1-two-sum/two-sum.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.ContainsKey(complement)) {
+                return new int[] { map[complement], i };
+            }
+            
+            if (!map.ContainsKey(nums[i])) {
+                map[nums[i]] = i;
+            }
+        }
+        
+        throw new ArgumentException("No two sum solution");
+    }
+}
